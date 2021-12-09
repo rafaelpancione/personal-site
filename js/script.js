9a6b2259b569350e08ad4de222678802bc6e1725
Rafael Pancione <rafaelpancione@hotmail.com>
2021-12-09 10:58:16 -0300

    init

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+function openSideBar(){
+    document.querySelector('.js-portifolio').style.width = "100%";
+    document.querySelector('.js-portifolio').style.display ="block";
+}
+
+function closeSideBar(){
+    document.querySelector('.js-portifolio').style.display = "block";
+    document.querySelector('.js-portifolio').style.width = "100px";
+}
+
